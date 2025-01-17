0bd24fd1195d2430d5121d9422bb7da66986c43e
Tom Dolan <tddzyx@gmail.com>
2025-01-17 15:23:38 -0500

    reverted

--- web/app/players/page.tsx
+++ web/app/players/page.tsx
@@ -1,29 +1,12 @@
-"use client";
-import { useEffect, useState } from "react";
 import { GetRoster } from "../components/actions";
 import PlayerSelector from "../components/player-selector";
-import { useRouter } from "next/navigation";
-import { Box } from "@mui/material";
+import { redirect } from "next/navigation";
 
-export const dynamic = 'force-dynamic';
-export const fetchCache = 'force-no-store';
-
-export default function Players() {
-  const [players, setPlayers] = useState<RosterPlayer[]>([]);
-  const router = useRouter();
-
-  useEffect(() => {
-    async function fetchPlayers() {
-      try {
-        const playersData = await GetRoster();
-        setPlayers(playersData);
-      } catch (e) {
-        router.push("/error");
-      }
-    }
-
-    fetchPlayers();
-  }, [router]);
-
-  return <PlayerSelector players={players} />;
+export default async function Players() {
+  try {
+    const players: RosterPlayer[] = await GetRoster();
+    return <PlayerSelector players={players} />;
+  } catch (e) {
+    redirect("/error");
+  }
 }
